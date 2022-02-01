ee826229fd75d91f7b201e23380277762c26e585
vanshaj810 <vanshaj810@gmail.com>
2022-02-01 14:15:30 +0530

    this is for git practice

---
+++ Primeno.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Primeno {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the limit that you want to enter:");
+        int t=sc.nextInt();
+        
+    }
+}
