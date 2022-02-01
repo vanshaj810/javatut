8b64245b57602ac268058988160df121ac7ffcc6
vanshaj810 <vanshaj810@gmail.com>
2022-02-01 15:07:31 +0530

    prime done

--- Primeno.java
+++ Primeno.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 
 public class Primeno {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the limit that you want to enter:");
-        int t=sc.nextInt();
-        
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int count = 0;
+            for (int j = 2; j * j <= n; j++) {
+                if (n % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                System.out.println("prime");
+            } else {
+                System.out.println("not prime");
+            }
+        }
     }
 }
