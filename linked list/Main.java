f03c88f3f3741127d1d26f66e8e256464ed1bf38
Vanshaj Bhardwaj <79003511+vanshaj810@users.noreply.github.com>
2022-05-29 14:58:30 +0530

    linked list started

---
+++ linked list/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    /**
+     * Node
+     */
+    public static class Node {
+        int data;
+        Node next;
+        
+    }
+    /**
+     * LinkedList
+     */
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
